7f02db672e01d115dd7c37f25d0ab6aa70575c09
zoax3155 <jan.stueven1@uni-oldenburg.de>
2023-07-14 14:24:36 +0200

    test use

--- src/lib/example.component.ts
+++ src/lib/example.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from "@angular/core";
+import { HttpContext } from "@angular/common/http";
+import { USE_API_URL, USE_LOADER } from "common";
 
 /** Minimalistic example Component. */
 @Component({
@@ -6,4 +8,12 @@ import {Component, OnInit} from "@angular/core";
 })
 export class ExampleComponent {
 
+  api_url: string = "/takeCsv";
+
+  value_name: string = "fileList";
+
+  httpContext: HttpContext = new HttpContext().set(USE_API_URL, true).set(USE_LOADER, true)
+
+
+  
 }
